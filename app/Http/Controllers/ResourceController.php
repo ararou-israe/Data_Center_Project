6b9f0ed81d697c27b3e8d96618d4e858ae18cd16
saadmarzouq <161056379+saadmr2@users.noreply.github.com>
2026-01-27 18:31:36 +0100

    Fix formatting and add missing newline at end of file

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -19,3 +19,34 @@ class ResourceController extends Controller
         return view('responsable.dashboard', compact('ressources'));
     }
 }
+
+
+
+// <?php
+
+// namespace App\Http\Controllers;
+
+// use App\Models\Ressource;
+
+// class ResourceController extends Controller
+// {
+//     /**
+//      * PUBLIC interface page
+//      * Used by / and /interface
+//      */
+//     public function index()
+//     {
+//         $ressources = Ressource::where('etat', '!=', 'indisponible')->get();
+//         return view('interface-main', compact('ressources'));
+//     }
+
+//     /**
+//      * Responsable dashboard
+//      */
+//     public function dashboard()
+//     {
+//         $ressources = Ressource::all();
+//         return view('responsable.dashboard', compact('ressources'));
+//     }
+// }
+ hada howa l code li khdamli hada li drti makhdamch ok 
